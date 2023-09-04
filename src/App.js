30d681da6e3e477ce950d7bc29761f7a8c09e96e
sharathps_007 <sharathpunjala75@gmail.com>
2023-09-04 17:42:54 +0530

    changes2

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Fun from './components/Fun';
 import Cv from './components/Cv';
 import Feed from './components/Feed';
 import Footer from './components/Footer';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
 
 function App() {
   return (
