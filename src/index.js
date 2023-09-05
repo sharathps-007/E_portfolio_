1cd20b10d60679ea166b2661d4c64262584461fc
sharathps_007 <sharathpunjala75@gmail.com>
2023-09-05 23:07:14 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { HashRouter } from 'react-router-dom';
 import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
