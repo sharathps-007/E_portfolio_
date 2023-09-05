c9990cbf5294da038044ca57d0ea730a0b8c1ae7
sharathps_007 <sharathpunjala75@gmail.com>
2023-09-05 23:43:25 +0530

    changes

--- src/components/Fun.jsx
+++ src/components/Fun.jsx
@@ -13,7 +13,7 @@ function Fun(){
     <div class="card-body">
       <h5 class="card-title">Simon Game</h5>
       <p class="card-text">A game that tests your focus, concentration and short-term memory.</p>
-      <a href="https://nitinsai381.github.io/Simon_Game/" class="btn btn-card" style={{fontSize:"1rem",backgroundColor:"aliceblue",color:"black",borderWidth:"0.1rem"}}>Play</a>
+      <a href="https://sharathps-007.github.io/Simon_Game/" class="btn btn-card" style={{fontSize:"1rem",backgroundColor:"aliceblue",color:"black",borderWidth:"0.1rem"}}>Play</a>
     </div>
   </div>
   <div class="card col-lg-3 col-md-4 col-sm-4 cardChanges"style={{backgroundColor:"rgb(65,65,65)",color:"aliceblue"}}>
@@ -23,7 +23,7 @@ function Fun(){
     <div class="card-body">
       <h5 class="card-title">Dice Challenge</h5>
       <p class="card-text">Toss dice by refreshing page, challenge with your friends.</p>
-      <a href="https://nitinsai381.github.io/dice_game/" class="btn btn-card" style={{fontSize:"1rem",backgroundColor:"aliceblue",color:"black",borderRadius:"0.5rem"}}>Toss it</a>
+      <a href="https://sharathps-007.github.io/Dice_Game/" class="btn btn-card" style={{fontSize:"1rem",backgroundColor:"aliceblue",color:"black",borderRadius:"0.5rem"}}>Toss it</a>
       
     </div>
     
@@ -35,7 +35,7 @@ function Fun(){
     <div class="card-body">
       <h5 class="card-title">Drum Kit</h5>
       <p class="card-text">By using mouse clicks or keys, play music and have fun.</p>
-      <a href="https://nitinsai381.github.io/Drum_Kit/" class="btn btn-card" style={{fontSize:"1rem",backgroundColor:"aliceblue",color:"black",borderWidth:"0.1rem"}}>Play</a>
+      <a href="https://sharathps-007.github.io/Drum_Kit/" class="btn btn-card" style={{fontSize:"1rem",backgroundColor:"aliceblue",color:"black",borderWidth:"0.1rem"}}>Play</a>
     </div>
   </div>
   </div>
